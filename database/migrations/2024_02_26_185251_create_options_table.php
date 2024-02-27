17f2b73d9de6ed1d8036ac6b63e463fc54bc24ab
laiavizcarro <145133090+laiavizcarro@users.noreply.github.com>
2024-02-27 15:55:06 +0100

    Modify create database

--- database/migrations/2024_02_26_185251_create_options_table.php
+++ database/migrations/2024_02_26_185251_create_options_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string("option");
             $table->boolean("isCorrect");
-            $table->id("questionId");
+            $table->integer("questionId");
         });
     }
 
--- database/migrations/2024_02_26_185401_create_tests_table.php
+++ database/migrations/2024_02_26_185401_create_tests_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('tests', function (Blueprint $table) {
             $table->id();
-            $table->id("studentId");
-            $table->id("licenseId");
-            $table->int("errors");
+            $table->integer("studentId");
+            $table->integer("licenseId");
+            $table->integer("errors");
         });
     }
 
