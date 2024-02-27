d428eac941038c3bf6cfe898461ebc7c43a4b759
Zhiou <Zhiou@prueba.com>
2024-02-27 17:21:28 +0100

    Delete the nav bar of the admin panel

--- resources/js/layouts/Authenticated.vue
+++ resources/js/layouts/Authenticated.vue
@@ -2,19 +2,6 @@
     <div class="layout-wrapper" :class="containerClass">
         <app-topbar></app-topbar>
         <div class="layout-main-container ">
-            <Breadcrumb :home="home" :model="crumbs" class="mb-2">
-                    <template #item="{ item, props }">
-                        <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
-                            <a :href="href" v-bind="props.action" class="btn btn-link"   @click="navigate">
-                                <span :class="[item.icon, 'text-color']" />
-                                <span class="text-primary font-semibold">{{ item.label }}</span>
-                            </a>
-                        </router-link>
-                        <a v-else :href="item.url" :target="item.target" v-bind="props.action">
-                            <span class="text-color">{{ item.label }}</span>
-                        </a>
-                    </template>
-                </Breadcrumb>
             <div class="layout-main">
                 
 
