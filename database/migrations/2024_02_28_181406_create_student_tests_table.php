3fdfadcb15f27c6bbea939301fc299bc6740734b
laiavizcarro <145133090+laiavizcarro@users.noreply.github.com>
2024-02-29 16:39:27 +0100

    Create student_test table

---
+++ database/migrations/2024_02_28_181406_create_student_tests_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_tests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("student_id");
+            $table->unsignedBigInteger("test_id");
+            $table->unsignedBigInteger("question_id");
+            $table->boolean("is_correct");
+            $table->date("start_date");
+            $table->date("end_date");
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+            $table->foreign('test_id')->references('id')->on('tests')->onDelete('cascade');
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_tests');
+    }
+};
