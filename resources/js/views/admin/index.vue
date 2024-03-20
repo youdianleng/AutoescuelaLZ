c4f8177106ef411ac928d7aec8fd44a787a7ae26
laiavizcarro <145133090+laiavizcarro@users.noreply.github.com>
2024-03-20 16:47:34 +0100

    Profile view

--- resources/js/views/admin/index.vue
+++ resources/js/views/admin/index.vue
@@ -19,50 +19,50 @@
                
             </div>
 
-            <div class="card col-11 d-flex">
-                <div class="col-12 justify-content-center">
-                    <h3 class="col-12 d-flex justify-content-center">Preguntas Creados</h3>
-                    <table class="col-12 mt-5">
-                        <tbody class="col-12">
-                            <tr class="col-12 border-top border-bottom bg-body-tertiary">
-                                <th class="col-3 text-center">Pregunta</th>
-                                <th class="col-3 text-center">Numero Opcion Correcta</th>
-                                <th class="col-3 text-center">Dificuldad</th>
-                                <th class="col-3 text-center">Fecha Creado</th>
-                            </tr>
-                            <tr v-for="student in students" class="col-12">
-                                <td class="col-3 text-center">{{ student.name }}</td>
-                                <td class="col-3 text-center">{{ student.surname }}</td>
-                                <td class="col-3 text-center">{{ student.email }}</td>
-                                <td class="col-3 text-center">{{ student.license_id }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+            <div v-if="role === 'teacher'">
+                <div class="card col-11 d-flex">
+                    <div class="col-12 justify-content-center">
+                        <h3 class="col-12 d-flex justify-content-center">Preguntas Creados</h3>
+                        <table class="col-12 mt-5">
+                            <tbody class="col-12">
+                                <tr class="col-12 border-top border-bottom bg-body-tertiary">
+                                    <th class="col-3 text-center">Pregunta</th>
+                                    <th class="col-3 text-center">Numero Opcion Correcta</th>
+                                    <th class="col-3 text-center">Dificuldad</th>
+                                    <th class="col-3 text-center">Fecha Creado</th>
+                                </tr>
+                                <tr v-for="student in students" class="col-12">
+                                    <td class="col-3 text-center">{{ student.name }}</td>
+                                    <td class="col-3 text-center">{{ student.surname }}</td>
+                                    <td class="col-3 text-center">{{ student.email }}</td>
+                                    <td class="col-3 text-center">{{ student.license_id }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    
                 </div>
-                
-            </div>
-
-            <div class="card col-11 d-flex">
-                <div class="col-12 justify-content-center">
-                    <h3 class="col-12 d-flex justify-content-center">Alumnos Asignados</h3>
-                    <table class="col-12 mt-5">
-                        <tbody class="col-12">
-                            <tr class="col-12 border-top border-bottom bg-body-tertiary">
-                                <th class="col-3 text-center">Nombre</th>
-                                <th class="col-3 text-center">Apellido</th>
-                                <th class="col-3 text-center">Email</th>
-                                <th class="col-3 text-center">Licencia</th>
-                            </tr>
-                            <tr v-for="student in students" class="col-12">
-                                <td class="col-3 text-center">{{ student.name }}</td>
-                                <td class="col-3 text-center">{{ student.surname }}</td>
-                                <td class="col-3 text-center">{{ student.email }}</td>
-                                <td class="col-3 text-center">{{ student.license_id }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                <div class="card col-11 d-flex">
+                    <div class="col-12 justify-content-center">
+                        <h3 class="col-12 d-flex justify-content-center">Alumnos Asignados</h3>
+                        <table class="col-12 mt-5">
+                            <tbody class="col-12">
+                                <tr class="col-12 border-top border-bottom bg-body-tertiary">
+                                    <th class="col-3 text-center">Nombre</th>
+                                    <th class="col-3 text-center">Apellido</th>
+                                    <th class="col-3 text-center">Email</th>
+                                    <th class="col-3 text-center">Licencia</th>
+                                </tr>
+                                <tr v-for="student in students" class="col-12">
+                                    <td class="col-3 text-center">{{ student.name }}</td>
+                                    <td class="col-3 text-center">{{ student.surname }}</td>
+                                    <td class="col-3 text-center">{{ student.email }}</td>
+                                    <td class="col-3 text-center">{{ student.license_id }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-                
             </div>
             
         </div>
