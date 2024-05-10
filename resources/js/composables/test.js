caa08ba92b3a2c5924fe55c57213b48b0ac14a70
Zhiou <Zhiou@prueba.com>
2024-05-10 13:09:14 +0200

    Move Test Codes to the composable

--- resources/js/composables/test.js
+++ resources/js/composables/test.js
@@ -3,6 +3,14 @@ import { useRouter } from 'vue-router'
 
 export default function useTest() {
     const test = ref([]);
+    // Set contador to jump between the questions
+    let contador = 0;
+
+    // Store all the questions have in this test
+    const questions = ref([]);
+
+    // There gonna store the question where the position of question is that indicated with contador
+    const questionId = ref({});
 
     const getFacilTest = async() =>{
 
@@ -82,7 +90,196 @@ export default function useTest() {
         });
     }
 
+let user_id;
+let route_id;
+let respuesta;
 
+const recibirRespuesta = async(Respuesta) =>{
+    respuesta = Respuesta;
+    console.log(Respuesta);
+}
+
+const buscarDatosTest = async(level,RouteId,userId) =>{
+    
+    user_id = userId;
+    route_id = RouteId;
+    axios.get('/api/question/' + level + '/' + RouteId)
+        .then(response => {
+            //save que question value
+            questions.value = response.data;
+
+            //Save the question in position 0 
+            questionId.value = questions.value[contador];
+
+            //call function to delete if this student already finish this test.
+            searchExist();
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+
+
+    // This array use for check the student pass the test or not
+let respuestasValidar = [];
+
+const compruebaDatos = async() =>{
+    if(respuestasValidar.length < questions.value.length){
+        showNextQuestion();
+
+        const radios = document.querySelectorAll('.switchColor1');
+        // Remover la clase 'switchColor2' de todos los radios
+
+        radios.forEach(radio => {
+            radio.parentNode.classList.remove('switchColor2');
+        });
+    }else{
+
+    }
+}
+
+// Function use for jump to the next question
+const showNextQuestion = async() =>{
+    // If the checkbox is not checked is gonna appear this alert
+    if(respuesta == ""){
+        alert("Debes elegir por menos 1 option");
+    }else{ 
+        // set the counter + 1 to jump to next question
+        contador = contador + 1;
+
+        // do an for with the option of this question
+        for(let i in questionId.value['options']){
+            // if the student have choice the correct answer 
+            if(respuesta == questionId.value['options'][i].id && questionId.value['options'][i].is_correct == 1){
+                // push in the respuetaValidar array 1 as correct
+
+                if(respuestasValidar[contador-1] !== undefined){
+                    respuestasValidar[contador-1] = 1;
+                    
+                    singleTestQuestionCompleteEdit(user_id,route_id,questionId.value['id'],respuesta,1);
+                    respuesta = "";
+                }else{
+                    respuestasValidar.push(1);
+                    singleTestQuestionCompleteSave(user_id,route_id,questionId.value['id'],respuesta,1);
+                    changeResultArray();
+                    respuesta = "";
+                }
+
+                // insert into the bdd the answer of this question
+                
+                break;
+            }else if(respuesta == questionId.value['options'][i].id && questionId.value['options'][i].is_correct == 0){
+                // push in the respuestaValidar array 0 as incorrect
+                if(respuestasValidar[contador-1] !== undefined){
+                    respuestasValidar[contador-1] = 0;
+                   
+                    singleTestQuestionCompleteEdit(user_id,route_id,questionId.value['id'],respuesta,0);
+                    respuesta = "";
+                }else{
+                    respuestasValidar.push(0);
+                    singleTestQuestionCompleteSave(user_id,route_id,questionId.value['id'],respuesta,0);
+                    changeResultArray();
+                    respuesta = "";
+                }
+
+                // insert into the bdd the answer of this question
+                
+                break;
+            }
+        }
+
+        // Check is there any quesiton more or not
+        if(questions.value[contador] == null){
+            // If this is the final question about this test, call the function
+            searchExist();
+        }else{
+            // If this is not the last question change the content of questionId to show
+            questionId.value = questions.value[contador];
+
+        }
+    }
+}
+
+const enviarButton = async(testId) =>{
+    if(respuestasValidar.length < testId){
+        alert("Realizar los preguntas anteriores");
+    }else{
+        questionId.value = questions.value[testId];
+        contador = testId;
+    }
+   
+}
+
+const previousQuestion = async() =>{
+    
+    questionId.value = questions.value[contador-1];
+    contador = contador - 1;
+}
+
+// Search in bbdd, did the user already have one test with this id.
+const searchExist = async() =>{
+    // if existe delete it
+    axios.delete('/api/test/exist/' + user_id + "/" + route_id)
+    .then(response => {
+        // after delete call the finalizar function to regist this time of record into the bbdd
+        finalizar();
+    })
+    .catch(function (error) {
+        console.log(error);
+        
+    });
+}
+
+
+
+// check at final that student have passed the test or not
+const finalizar = async() =>{
+
+    // Variable use for count the correct answer of the student
+    let pass = 0;
+
+    // This is use for boolean, if student have more than 3 answer = correct this will be set at 1
+    let passed = 1;
+
+    // make a loop with respuetasValidar array
+    for(let count in respuestasValidar){
+        
+        // if there are 1 in the position of array
+        if(respuestasValidar[count] == 0){
+            // add 1 to the pass
+            pass = pass + 1;
+        }
+    }
+   
+    // if pass is more or equal 3 set the passed to 1
+    if(pass >= 3){
+        passed = 0;
+    }
+    
+    // final step of test send the result of the test to bbdd
+    finalizarValue(user_id,route_id,passed,questionId.value['difficulty']);
+    if(respuestasValidar.length > 1){
+        let respuestasUno = respuestasValidar.filter(function(respuesta) {
+            return respuesta === 0;
+        });
+        let cantidadDeZeros = respuestasUno.length;
+
+        let arrayCodificado = btoa(JSON.stringify(respuestasValidar));
+        window.location.href = '/Final/'+ user_id + '/' + route_id + '/' + cantidadDeZeros + '/' + arrayCodificado;
+    }
+    
+}
+
+const changeResultArray = () =>{
+    if(respuestasValidar[contador-1] != undefined){
+        // Obtener el botón con la clase 'buttonCard'
+        const button = document.querySelector('.cardButton');
+
+        // Reemplazar la clase 'buttonCard' con 'buttonCard1'
+        button.classList.replace('cardButton', 'cardButton2');
+    }
+}
 
     return{
         test,
@@ -92,6 +289,18 @@ export default function useTest() {
         searchExistTestQuestion,
         finalizarValue,
         singleTestQuestionCompleteSave,
-        singleTestQuestionCompleteEdit
+        singleTestQuestionCompleteEdit,
+        compruebaDatos,
+        enviarButton,
+        previousQuestion,
+        buscarDatosTest,
+        recibirRespuesta,
+        changeResultArray,
+        respuestasValidar,
+        questions,
+        questionId,
+        contador
+
+
     }
 }
--- resources/js/views/test/index.vue
+++ resources/js/views/test/index.vue
@@ -65,22 +65,16 @@ import { setLocale } from 'yup';
 import axios from 'axios';
 import { useStore } from 'vuex';
 import useTest from "@/composables/test";
+import { watchEffect } from "vue";
 
-const { searchExistTestQuestion,finalizarValue,singleTestQuestionCompleteSave,singleTestQuestionCompleteEdit } = useTest();
+const { searchExistTestQuestion,finalizarValue,singleTestQuestionCompleteSave,singleTestQuestionCompleteEdit,compruebaDatos,enviarButton,previousQuestion,buscarDatosTest,respuestasValidar,questions,questionId,contador,recibirRespuesta } = useTest();
 
 const store = useStore();
 const user = computed(() => store.state.auth.user)
 const role = computed(() => store.state.auth.role)
 const route = useRoute()
 
-// Set contador to jump between the questions
-let contador = 0;
 
-// Store all the questions have in this test
-const questions = ref([]);
-
-// There gonna store the question where the position of question is that indicated with contador
-const questionId = ref({});
 
 // Take the value of the input that have name as respuesta
 const { value: respuesta } = useField('respuesta', null, { initialValue: '' });
@@ -97,171 +91,18 @@ onMounted(() => {
     searchExistTestQuestion(user.value['user_id'],route.params.id);
 
     // Find the question have in this test
-    axios.get('/api/question/' + route.params.level + '/' + route.params.id)
-        .then(response => {
-            //save que question value
-            questions.value = response.data;
-
-            //Save the question in position 0 
-            questionId.value = questions.value[contador];
-
-            //call function to delete if this student already finish this test.
-            searchExist();
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+    buscarDatosTest(route.params.level, route.params.id,user.value['user_id'],respuestas.respuesta);
 })
 
-// This array use for check the student pass the test or not
-let respuestasValidar = [];
-
-const compruebaDatos = () =>{
-    if(respuestasValidar.length < questions.value.length){
-        showNextQuestion();
-
-        const radios = document.querySelectorAll('.switchColor1');
-        // Remover la clase 'switchColor2' de todos los radios
-
-        radios.forEach(radio => {
-            radio.parentNode.classList.remove('switchColor2');
-        });
-    }else{
-
+watchEffect(() => {
+    // Accedemos al valor de 'respuesta.respuesta' dentro de 'respuesta'
+    const respuestaValor = respuestas.respuesta;
+    // Verificamos si 'respuesta.respuesta' ha cambiado
+    if (respuestaValor !== '') {
+    // Llamamos a 'recibirRespuesta' con el valor de 'respuesta.respuesta'
+    recibirRespuesta(respuestaValor);
     }
-}
-
-// Function use for jump to the next question
-const showNextQuestion = () =>{
-    // If the checkbox is not checked is gonna appear this alert
-    if(respuestas.respuesta == ""){
-        alert("Debes elegir por menos 1 option");
-    }else{ 
-        // set the counter + 1 to jump to next question
-        contador = contador + 1;
-
-        // do an for with the option of this question
-        for(let i in questionId.value['options']){
-            // if the student have choice the correct answer 
-            if(respuestas.respuesta == questionId.value['options'][i].id && questionId.value['options'][i].is_correct == 1){
-                // push in the respuetaValidar array 1 as correct
-
-                if(respuestasValidar[contador-1] !== undefined){
-                    respuestasValidar[contador-1] = 1;
-                    
-                    singleTestQuestionCompleteEdit(user.value['user_id'],route.params.id,questionId.value['id'],respuestas.respuesta,1);
-                    respuestas.respuesta = "";
-                }else{
-                    respuestasValidar.push(1);
-                    singleTestQuestionCompleteSave(user.value['user_id'],route.params.id,questionId.value['id'],respuestas.respuesta,1);
-                    changeResultArray();
-                    respuestas.respuesta = "";
-                }
-
-                // insert into the bdd the answer of this question
-                
-                break;
-            }else if(respuestas.respuesta == questionId.value['options'][i].id && questionId.value['options'][i].is_correct == 0){
-                // push in the respuestaValidar array 0 as incorrect
-                if(respuestasValidar[contador-1] !== undefined){
-                    respuestasValidar[contador-1] = 0;
-                   
-                    singleTestQuestionCompleteEdit(user.value['user_id'],route.params.id,questionId.value['id'],respuestas.respuesta,0);
-                    respuestas.respuesta = "";
-                }else{
-                    respuestasValidar.push(0);
-                    singleTestQuestionCompleteSave(user.value['user_id'],route.params.id,questionId.value['id'],respuestas.respuesta,0);
-                    changeResultArray();
-                    respuestas.respuesta = "";
-                }
-
-                // insert into the bdd the answer of this question
-                
-                break;
-            }
-        }
-
-        // Check is there any quesiton more or not
-        if(questions.value[contador] == null){
-            // If this is the final question about this test, call the function
-            searchExist();
-        }else{
-            // If this is not the last question change the content of questionId to show
-            questionId.value = questions.value[contador];
-
-        }
-    }
-}
-
-const enviarButton = (testId) =>{
-    if(respuestasValidar.length < testId){
-        alert("Realizar los preguntas anteriores");
-    }else{
-        questionId.value = questions.value[testId];
-        contador = testId;
-    }
-   
-}
-
-const previousQuestion = () =>{
-    
-    questionId.value = questions.value[contador-1];
-    contador = contador - 1;
-}
-
-// Search in bbdd, did the user already have one test with this id.
-const searchExist = () =>{
-    // if existe delete it
-    axios.delete('/api/test/exist/' + user.value['user_id'] + "/" + route.params.id)
-    .then(response => {
-        // after delete call the finalizar function to regist this time of record into the bbdd
-        finalizar();
-    })
-    .catch(function (error) {
-        console.log(error);
-        
-    });
-}
-
-
-
-// check at final that student have passed the test or not
-const finalizar = () =>{
-
-    // Variable use for count the correct answer of the student
-    let pass = 0;
-
-    // This is use for boolean, if student have more than 3 answer = correct this will be set at 1
-    let passed = 1;
-
-    // make a loop with respuetasValidar array
-    for(let count in respuestasValidar){
-        
-        // if there are 1 in the position of array
-        if(respuestasValidar[count] == 0){
-            // add 1 to the pass
-            pass = pass + 1;
-        }
-    }
-   
-    // if pass is more or equal 3 set the passed to 1
-    if(pass >= 3){
-        passed = 0;
-    }
-    
-    // final step of test send the result of the test to bbdd
-    finalizarValue(user.value['user_id'],route.params.id,passed,questionId.value['difficulty']);
-    if(respuestasValidar.length > 1){
-        let respuestasUno = respuestasValidar.filter(function(respuesta) {
-            return respuesta === 0;
-        });
-        let cantidadDeZeros = respuestasUno.length;
-
-        let arrayCodificado = btoa(JSON.stringify(respuestasValidar));
-        window.location.href = '/Final/'+ user.value['user_id'] + '/' + route.params.id + '/' + cantidadDeZeros + '/' + arrayCodificado;
-    }
-    
-}
+});
 
 const changeColorButton = (event) => {
     const radios = document.querySelectorAll('.switchColor1');
@@ -275,15 +116,7 @@ const changeColorButton = (event) => {
 };
 
 
-const changeResultArray = () =>{
-    if(respuestasValidar[contador-1] != undefined){
-        // Obtener el botón con la clase 'buttonCard'
-        const button = document.querySelector('.cardButton');
 
-        // Reemplazar la clase 'buttonCard' con 'buttonCard1'
-        button.classList.replace('cardButton', 'cardButton2');
-    }
-}
 
 </script>
 <style>
